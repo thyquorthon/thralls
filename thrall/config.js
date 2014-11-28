58798fe4996f02fe27a5f5b3f59fa7f4a9db6910
Quorthon <jorge@bcnbynight.com>
2014-11-28 18:57:11 +0100

    initial commit

---
+++ thrall/config.js
@@ -0,0 +1,12 @@
+var config = {};
+// Logging active (true|false);
+config.log = true;
+// Log separator
+config.logSep = '\n---------------------------------------------'
+// Listening PORT
+config.PORT = 8001;
+// Master address
+config.MASTER = 'localhost:8000';
+
+// Return module.
+module.exports = config;
