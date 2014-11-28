a4927c86e6ce6b367a518863f64d650f65edc570
Quorthon <jorge@bcnbynight.com>
2014-11-28 18:53:37 +0100

    initial commit

---
+++ Server/config.js
@@ -0,0 +1,10 @@
+var config = {};
+// Logging active (true|false);
+config.log = true;
+// Log separator
+config.logSep = '\n---------------------------------------------'
+// Listening PORT
+config.PORT = 8000;
+
+// Return module.
+module.exports = config;
